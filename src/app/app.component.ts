dfbf80c0592ba0eb62cc055cb3b08840ace965f3
dorpeled <dpeled@rekor.ai>
2025-04-07 11:08:50 +0300

    last update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,3 @@
-import {
-  animate,
-  group,
-  query,
-  style,
-  transition,
-  trigger,
-} from '@angular/animations';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './shared/nav/nav.component';
@@ -16,49 +8,6 @@ import { NavComponent } from './shared/nav/nav.component';
   imports: [RouterOutlet, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  animations: [
-    trigger('routeAnimations', [
-      transition('* <=> *', [
-        // Set initial states for entering and leaving pages
-        query(
-          ':enter',
-          [
-            style({
-              opacity: 0,
-              position: 'absolute',
-              width: '100%',
-            }),
-          ],
-          { optional: true }
-        ),
-
-        query(
-          ':leave',
-          [
-            style({
-              opacity: 1,
-              position: 'absolute',
-              width: '100%',
-            }),
-          ],
-          { optional: true }
-        ),
-
-        // Run animations in parallel
-        group([
-          // Fade out the leaving page
-          query(':leave', [animate('300ms ease-out', style({ opacity: 0 }))], {
-            optional: true,
-          }),
-
-          // Fade in the entering page
-          query(':enter', [animate('500ms ease-in', style({ opacity: 1 }))], {
-            optional: true,
-          }),
-        ]),
-      ]),
-    ]),
-  ],
 })
 export class AppComponent {
   title = 'Angular Animations Playground';
